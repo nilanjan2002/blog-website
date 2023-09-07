8fe4393bb51e1c6a2fa869752bbcd907d532be86
Nilanjan Sengupta <nilanjansengupta471@gmail.com>
2023-09-08 01:53:18 +0530

    Add GET:id route & FIX article not defined bug

--- routes/articles.js
+++ routes/articles.js
@@ -3,13 +3,27 @@ const articleRouter = require('express').Router();
 const Article = require('../models/model');
 
 async function main(){
-    const db = await mongoose.connect('mongodb://localhost:27017/Demo');
+    const db = await mongoose.connect('mongodb://localhost:27017');
 }
 main().catch(err=> console.log(err));
 
 articleRouter.get('/new',(req,res)=>{
-res.render('articles/new');
+res.render('articles/new', {article: new Article().toObject()});
 })
+
+articleRouter.get('/:id', async (req, res) => {
+    try {
+      const result = await Article.findById(req.params.id);
+      if (!result) {
+        return res.status(404).json({ message: 'Article not found' });
+      }
+      res.json(result);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Internal server error' });
+    }
+  });
+  
 
 articleRouter.post('/',async (req,res)=>{
     const{title, description,genre } = req.body;
@@ -19,8 +33,14 @@ articleRouter.post('/',async (req,res)=>{
         genre   : genre,
         date: new Date().toLocaleDateString()
     })
-    await article.save()
-    res.redirect('/');
+    try {
+        const articleID = await article.save()
+        console.log(articleID)
+        res.redirect(`articles/${articleID._id}`);
+    } catch (error) {
+        res.render('articles/new',{ article:article.toObject()})
+    }
+    // res.redirect('/');
 })
 
 
